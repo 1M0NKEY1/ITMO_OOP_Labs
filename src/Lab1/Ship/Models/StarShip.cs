df5d4cc0db2d67a5112272ce909b26b37ba21d9b
Egor <fordgt5735@gmail.com>
2023-10-06 19:22:43 +0300

    feat: add enum Obstacles and StarShip

---
+++ src/Lab1/Ship/Models/StarShip.cs
@@ -0,0 +1,15 @@
+﻿namespace Itmo.ObjectOrientedProgramming.Lab1.Ship.Models;
+
+public abstract class StarShip
+{
+    public bool Crew { get; set; }
+    public int ClassOfDeflectors { get; protected init; }
+    public int ClassOfHull { get; protected init; }
+    public int ClassOfEngine { get; protected init; }
+    public int ClassOfJumpEngine { get; protected init; }
+    public int Size { get; protected init; }
+    public bool Destroyed { get; set; }
+    public bool Emitter { get; set; }
+    public bool PhotonDeflector { get; set; }
+    public abstract void Destroy();
+}
