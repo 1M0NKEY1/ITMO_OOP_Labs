8c487686ef130a15a47360052c29a7c1a209ee80
Egor <fordgt5735@gmail.com>
2023-11-13 19:33:47 +0300

    feat: Was created Parser and Commands

---
+++ src/Lab4/CommandParser.cs
@@ -0,0 +1,20 @@
+﻿namespace Itmo.ObjectOrientedProgramming.Lab4;
+
+public class CommandParser
+{
+    private string _input;
+    private int _position;
+
+    public CommandParser(string input)
+    {
+        _input = input;
+        _position = 0;
+    }
+
+    public void Parse()
+    {
+        while (_position < _input.Length)
+        {
+        }
+    }
+}
---
+++ src/Lab4/Commands/CommandsWithFile/RenameFile.cs
@@ -0,0 +1,6 @@
+﻿namespace Itmo.ObjectOrientedProgramming.Lab4.Commands.CommandsWithFile;
+
+public class RenameFile
+{
+    
+}
