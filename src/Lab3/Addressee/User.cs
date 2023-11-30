a1464ba181edbc9ca2de677b35efb1dca859599b
Egor <fordgt5735@gmail.com>
2023-11-30 22:26:51 +0300

    fix: fixes with lists

--- src/Lab3/Addressee/User.cs
+++ src/Lab3/Addressee/User.cs
@@ -4,7 +4,6 @@ namespace Itmo.ObjectOrientedProgramming.Lab3.Addresse;
 
 public class User : IUser
 {
-    private readonly IList<IMessage> _listOfReadMessage = new List<IMessage>();
     private readonly IList<IMessage> _listOfUnreadMessages = new List<IMessage>();
 
     public void SaveMessage(IMessage message)
@@ -14,15 +13,9 @@ public class User : IUser
 
     public void ChangeStatus(IMessage message)
     {
-        if (_listOfReadMessage.Contains(message))
-        {
-            _listOfReadMessage.Remove(message);
-            _listOfUnreadMessages.Add(message);
-        }
-        else if (_listOfUnreadMessages.Contains(message))
+        if (_listOfUnreadMessages.Contains(message))
         {
             _listOfUnreadMessages.Remove(message);
-            _listOfReadMessage.Add(message);
         }
     }
 }
