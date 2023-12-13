378fa4079de83f5347963c3137227d08173a7453
Egor <fordgt5735@gmail.com>
2023-12-14 02:21:36 +0300

    fix: ServiceCollectionExtensions.cs

--- src/Lab5/Lab5.Application/Extensions/ServiceCollectionExtensions.cs
+++ src/Lab5/Lab5.Application/Extensions/ServiceCollectionExtensions.cs
@@ -9,15 +9,10 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddApplication(this IServiceCollection collection)
     {
         collection.AddScoped<IUserService, UserService>();
-        collection.AddScoped<IAdminService, AdminService>();
 
         collection.AddScoped<CurrentUserManager>();
         collection.AddScoped<ICurrentUserService>(
             p => p.GetRequiredService<CurrentUserManager>());
-
-        collection.AddScoped<CurrentAdminManager>();
-        collection.AddScoped<ICurrentAdminService>(
-            p => p.GetRequiredService<CurrentAdminManager>());
 
         return collection;
     }
